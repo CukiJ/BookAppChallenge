718c86f11506a5525725484d51c5ad13b09e3639
Jirike Cuki <sulak.jiri@gmail.com>
2021-04-11 10:37:48 +0200

    Initial Commit

---
+++ L15Ch/L15ChApp.swift
@@ -0,0 +1,17 @@
+//
+//  L15ChApp.swift
+//  L15Ch
+//
+//  Created by Jiří Šulák on 11.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct L15ChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
