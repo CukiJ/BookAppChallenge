37038314f3811a503b376e6c41c8e7a6ff92a640
Jirike Cuki <sulak.jiri@gmail.com>
2021-04-24 16:43:47 +0200

    Add star.fill to BookViewRow if isFavourite

--- L15Ch/Views/BookViewRow.swift
+++ L15Ch/Views/BookViewRow.swift
@@ -21,21 +21,41 @@ struct BookViewRow: View {
                 .shadow(radius: 20)
             
             VStack (alignment: .leading){
-                Text(book.title)
-                    .font(.title)
-                    .bold()
-                    .padding(5)
+                if book.isFavourite {
+                    
+                    HStack {
+                        Text(book.title)
+                            .font(.title)
+                            .bold()
+                            .padding(5)
+                        Spacer()
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30)
+                            .foregroundColor(.yellow)
+                            .padding(.trailing, 10)
+                    }
+                    .padding(.horizontal, 10)
+                    .frame(width: 320)
+                }
+                else {
+                    Text(book.title)
+                        .font(.title)
+                        .bold()
+                        .padding(10)
+                }
                 Text(book.author)
                     .italic()
                     .font(.body)
-                    .padding(.horizontal, 10)
+                    .padding(.horizontal, 15)
                 NavigationLink(
                     destination: BookDetailView(book: book),
                     label: {
                         Image("cover" + String(book.id))
                             .resizable()
                             .scaledToFit()
-                            .padding([.horizontal,.bottom], 10)
+                            .padding([.horizontal,.bottom], 5)
                             .frame(width: 320, height: 380)
                     })
             }
